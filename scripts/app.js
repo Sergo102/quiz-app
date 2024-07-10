7a7270576db1c32892b7425174565590a3bf12fb
Sergiy <barsulsergij@gmail.com>
2024-07-10 13:05:08 -0800

    Finished project

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,12 @@
 const questions = [
-{
-      question: 'Inside which HTML element do we put the JavaScript?',
-      answerA: '&ltjs&gt',
-      answerB: '&ltjavascript&gt',
-      answerC: '&ltscripting&gt',
+  {
+    question: 'Inside which HTML element do we put the JavaScript?',
+    answerA: '&ltjs&gt',
+    answerB: '&ltjavascript&gt',
+    answerC: '&ltscripting&gt',
     answerD: '&ltscript&gt',
-    correctAnswer: '&ltscript&gt',
+    correctAnswer: 'answerD',
+    selectedAnswer: '',
   },
   {
     question: 'What is the correct JavaScript syntax to change the content of the HTML element below?',
@@ -13,7 +14,8 @@ const questions = [
     answerB: '#demo.innerHTML = "Hello World!";',
     answerC: 'document.getElementByName("p").innerHTML = "Hello World!";',
     answerD: 'document.getElementById("demo").innerHTML = "Hello World!";',
-    correctAnswer: 'document.getElementById("demo").innerHTML = "Hello World!";',
+    correctAnswer: 'answerD',
+    selectedAnswer: '',
   },
   {
     question: 'Where is the correct place to insert a JavaScript?',
@@ -21,7 +23,8 @@ const questions = [
     answerB: 'The &lthead&gt section',
     answerC: 'The &ltbody&gt sections',
     answerD: 'Both the &lthead&gt and the &ltbody&gt section are correct',
-    correctAnswer: 'Both sections the &lthead&gt and the &ltbody" section are correct',
+    correctAnswer: 'answerD',
+    selectedAnswer: '',
   },
   {
     question: 'How do you create a function in JavaScript?',
@@ -29,7 +32,8 @@ const questions = [
     answerB: 'function myFunction();',
     answerC: 'function:myFunction',
     answerD: 'create myFunction()',
-    correctAnswer: 'function myFunction();',
+    correctAnswer: 'answerB',
+    selectedAnswer: '',
   },
   {
     question: 'How to write an IF statement in JavaScript?',
@@ -37,7 +41,8 @@ const questions = [
     answerB: 'if (i == 5)',
     answerC: 'if i = 5 then',
     answerD: 'if i = 5',
-    correctAnswer: 'if (i == 5)',
+    correctAnswer: 'answerB',
+    selectedAnswer: '',
   },
   {
     question: 'How can you add a comment in a JavaScript?',
@@ -45,7 +50,8 @@ const questions = [
     answerB: '//This is a comment',
     answerC: "'This is a comment",
     answerD: 'This is a comment',
-    correctAnswer: '//This is a comment',
+    correctAnswer: 'answerB',
+    selectedAnswer: '',
   },
   {
     question: 'What is correct way to write a JavaScript array?',
@@ -53,7 +59,8 @@ const questions = [
     answerB: 'var colors = [1:"red"< 2:"green", 3:"blue"];',
     answerC: 'var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue");',
     answerD: 'var colors = "red", "green", "blue";',
-    correctAnswer: 'var colors = ["red", "green", "blue"];',
+    correctAnswer: 'answerA',
+    selectedAnswer: '',
   },
   {
     question: 'What is the correct JavaScript syntax for opening a new window called "w2"?',
@@ -61,7 +68,8 @@ const questions = [
     answerB: 'w2 = window.open("http://www.w3schools.com");',
     answerC: 'w2 = new Window.open();',
     answerD: 'w2.open();',
-    correctAnswer: 'w2 = window.open("http://www.w3schools.com");',
+    correctAnswer: 'answerB',
+    selectedAnswer: '',
   },
   {
     question: 'How do you declare a JavaScript variable?',
@@ -69,7 +77,8 @@ const questions = [
     answerB: 'v carName;',
     answerC: 'variable carName;',
     answerD: 'createVariable carName;',
-    correctAnswer: 'var carName;',
+    correctAnswer: 'answerA',
+    selectedAnswer: '',
   },
   {
     question: "What ISN'T a JavaScript data type?",
@@ -77,88 +86,103 @@ const questions = [
     answerB: 'object',
     answerC: 'not found',
     answerD: 'undefined',
-    correctAnswer: 'not found',
+    correctAnswer: 'answerC',
+    selectedAnswer: '',
   },
 ];
 
-  let randomizedQuestions = questions
+let randomizedQuestions = questions
   .map(value => ({ value, sort: Math.random() }))
   .sort((a, b) => a.sort - b.sort)
   .map(({ value }) => value);
 
-  console.log(randomizedQuestions);
+console.log(randomizedQuestions);
+console.log("If you want to know the answers, just click on the array higher")
 
 
-  let h2 = document.querySelector('h2');
-  let answerA = document.querySelector("#answerA");
-  let answerB = document.querySelector("#answerB");
-  let answerC = document.querySelector("#answerC");
-  let answerD = document.querySelector("#answerD");
+let h2 = document.querySelector('h2');
+let answerA = document.querySelector("#answerA");
+let answerB = document.querySelector("#answerB");
+let answerC = document.querySelector("#answerC");
+let answerD = document.querySelector("#answerD");
   
-  let number = 0;
+let number = 0;
   
-  h2.innerText = randomizedQuestions[number].question;
-  answerA.innerHTML = `${randomizedQuestions[number].answerA}`;  
-  answerB.innerHTML = `${randomizedQuestions[number].answerB}`;
-  answerC.innerHTML = `${randomizedQuestions[number].answerC}`;
-  answerD.innerHTML = `${randomizedQuestions[number].answerD}`;
+h2.innerText = randomizedQuestions[number].question;
+answerA.innerHTML = `${randomizedQuestions[number].answerA}`;  
+answerB.innerHTML = `${randomizedQuestions[number].answerB}`;
+answerC.innerHTML = `${randomizedQuestions[number].answerC}`;
+answerD.innerHTML = `${randomizedQuestions[number].answerD}`;
   
-  let lis = document.querySelectorAll('li');
+let lis = document.querySelectorAll('li');
   
-  function selectAnswer(option){
-    lis[option].className = 'selected';
-    for(let i = 0;i < lis.length;i++){
-      if(lis[i] !== lis[option]){
-        lis[i].className = '';
-      }
-    }
-  }
-  
-  /* The part where you display question & answers */
-  
-  function changeQuestion(){
-    for(let i = 0;i < lis.length;i++){
-      if(lis[i].classList.length > 0){
-        nextQuestion();
-        unselect();
-        return;
-      }
-    }
-    alert('Please choose an answer');
-  }
-
-  function unselect(){
-    for(let i = 0;i < lis.length;i++){
+function selectAnswer(option){
+  lis[option].className = 'selected';
+  for(let i = 0;i < lis.length;i++){
+    if(lis[i] !== lis[option]){
       lis[i].className = '';
+    }else{
+      // Displays id of a selected <li> as a string
+      randomizedQuestions[number].selectedAnswer = lis[i].id;
     }
   }
-
-  function nextQuestion(){
-    if(number == 8){
-      document.querySelector('button').innerText = 'Submit ˃';
-      number += 1;
-      h2.innerText = randomizedQuestions[number].question;
-      answerA.innerHTML = `${randomizedQuestions[number].answerA}`;  
-      answerB.innerHTML = `${randomizedQuestions[number].answerB}`;
-      answerC.innerHTML = `${randomizedQuestions[number].answerC}`;
-      answerD.innerHTML = `${randomizedQuestions[number].answerD}`; 
-      return;
-    }else if(number < 9){
-      number += 1;
-      h2.innerText = randomizedQuestions[number].question;
-      answerA.innerHTML = `${randomizedQuestions[number].answerA}`;  
-      answerB.innerHTML = `${randomizedQuestions[number].answerB}`;
-      answerC.innerHTML = `${randomizedQuestions[number].answerC}`;
-      answerD.innerHTML = `${randomizedQuestions[number].answerD}`; 
-      return;
-    }else if(number == 9){
-      // The code lower won't change the question but is still needed
-      number++;
-    }else{
-      alert('Do you want to see your results');
-    }
-  };
+}
   
-  function showAnswer(){};
+/* The part where you display question & answers */
+  
+function changeQuestion(){
+  for(let i = 0;i < lis.length;i++){
+    if(lis[i].classList.length > 0){
+      nextQuestion();
+      unselect();
+      return;
+    }
+  }
+  alert('Please choose an answer');
+}
 
-  function showResults(){}
+function unselect(){
+  for(let i = 0;i < lis.length;i++){
+    lis[i].className = '';
+  }
+}
+
+function nextQuestion(){
+  if(number == 8){
+    document.querySelector('button').innerText = 'Submit ˃';
+    number += 1;
+    h2.innerText = randomizedQuestions[number].question;
+    answerA.innerHTML = `${randomizedQuestions[number].answerA}`;  
+    answerB.innerHTML = `${randomizedQuestions[number].answerB}`;
+    answerC.innerHTML = `${randomizedQuestions[number].answerC}`;
+    answerD.innerHTML = `${randomizedQuestions[number].answerD}`; 
+    return;
+  }else if(number < 9){
+    number += 1;
+    h2.innerText = randomizedQuestions[number].question;
+    answerA.innerHTML = `${randomizedQuestions[number].answerA}`;  
+    answerB.innerHTML = `${randomizedQuestions[number].answerB}`;
+    answerC.innerHTML = `${randomizedQuestions[number].answerC}`;
+    answerD.innerHTML = `${randomizedQuestions[number].answerD}`; 
+    return;
+  }else if(number == 9){
+    if(randomizedQuestions[number].selectedAnswer !== ''){
+      showResults();
+    }
+  }
+};
+
+function showResults(){
+  let correctAnswers = 0;
+  for(let i = 0;i < randomizedQuestions.length;i++){
+    if(randomizedQuestions[i].selectedAnswer == randomizedQuestions[i].correctAnswer){
+      correctAnswers++;
+      console.log(`${i} - Correct`);
+    }else{
+      console.log(`${i} - Wrong`)
+    }
+  }
+  alert(`${correctAnswers} out of 10 answers are correct`);
+  alert('The page will refresh');
+  location.reload();
+};
